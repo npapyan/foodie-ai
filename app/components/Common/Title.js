6cc6b339055351781bf90b08124050ca9d64f03e
npapyan <nickpapyan@proton.me>
2024-08-06 20:36:00 -0400

    Updated title.

--- app/components/Common/Title.js
+++ app/components/Common/Title.js
@@ -2,8 +2,8 @@ export default function Title() {
     return (
         <div>
             <div className="mb-4 gradient-text text-center py-2 px-2">
-            <h1 className="text-4xl">Is It Healthy?</h1>
-            <h2 className="text-2xl">Analyze a nutrition fact label with AI</h2>
+                <h1 className="text-4xl">Is It Healthy?</h1>
+                <h2 className="text-2xl">Nutrition analysis powered by Google Gemini</h2>
             </div>
         </div>
     )
